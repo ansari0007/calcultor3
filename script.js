5d930c4ccc4703474c8e763dc46faeb951fe71c2
Najmuddin <47290855+ansari0007@users.noreply.github.com>
2020-03-11 15:27:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function insert(a){
+	
+	var x=a;
+	
+		
+	if(x==='='){
+		
+		
+		document.getElementById('disp').value=eval(document.getElementById('disp').value);
+		
+		
+	}
+	else if(x==='c'){
+		
+		document.getElementById('disp').value='';
+		
+	}
+	else{
+		
+	var y=document.getElementById('disp');
+		y.value+=a;	
+		y.style.color='blue';
+		y.style.fontSize='1.5em';	
+		
+	}
+	
+	
+	
+	
+
+	
+}
